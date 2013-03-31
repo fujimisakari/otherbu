f10eaf8dbb37ed4fa8455f461d1ee51d5bbbb165
Ryo Fujimoto <fujimisakari@gmail.com>
2013-03-31 16:05:46 +0900

    テンプレート修正

--- module/root/views.py
+++ module/root/views.py
@@ -6,6 +6,8 @@ from django.conf import settings
 
 
 def index(request):
+    if request.session.get('DEMO_PAGE', False):
+        del request.session['DEMO_PAGE']
     params = {
         'title': settings.ROOT_TITLE,
         'body_padding': settings.PORTAL_BODY_PADDING,
