8f988e83ef1e31f140dd063034cedda5959e2ed4
Ryo Fujimoto <ryo.fujimoto@gu3.co.jp>
2015-07-26 00:22:21 +0900

    urls更新

--- module/oauth/urls.py
+++ module/oauth/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns(
     'module.oauth.views',
     url(r'callback/(?P<auth_type>\w+)/$', 'callback', name='auth_callback'),
     url(r'completion/$', 'completion_client', name='completion_client'),
-    url(r'(?P<auth_type>\w+)/client/$', 'login_client', name='auth_login_client'),
+    url(r'login/(?P<auth_type>\w+)/client/$', 'login_client', name='auth_login_client'),
     url(r'(?P<auth_type>\w+)/$', 'login', name='auth_login'),
 )
