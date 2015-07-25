274771482ac4bf9a755cce89a04403f119dbf342
Ryo Fujimoto <ryo.fujimoto@gu3.co.jp>
2015-07-26 03:01:07 +0900

    テンプレート修正

--- module/root/views.py
+++ module/root/views.py
@@ -3,14 +3,11 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.conf import settings
-from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
 
 
 def index(request):
-    if request.session.get('CLIENT', False):
-        del request.session['CLIENT']
-        return HttpResponseRedirect(reverse('completion_client'))
+    if request.session.get('DEMO_PAGE', False):
+        del request.session['DEMO_PAGE']
     params = {
         'title': settings.ROOT_TITLE,
         'body_padding': settings.PORTAL_BODY_PADDING,
