ef67d53a337a5fc97b79148ffc136501c3ebd672
Ryo Fujimoto <ryo.fujimoto@gu3.co.jp>
2015-08-03 03:38:35 +0900

    helpのurls更新

--- module/help/urls.py
+++ module/help/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns(
     'module.help.views',
-    url(r'^$', 'client_index', name='client_help_index'),
+    url(r'^client/$', 'client_index', name='client_help_index'),
 )
