96a12aaac13b146828a414ee623d4d9ebe15535b
Fujimisakari <fujimisakari@gmail.com>
2017-01-10 13:44:12 +0900

    modify settings init

--- application/settings/__init__.py
+++ application/settings/__init__.py
@@ -2,7 +2,7 @@
 
 import os
 
-if os.environ.get('APP_ENV') == 'dev':
+if os.environ.get('APP_ENV') == 'local' or os.environ.get('APP_ENV') == 'dev':
     from settings.dev_settings import *
 elif os.environ.get('APP_ENV') == 'staging':
     from settings.staging_settings import *
