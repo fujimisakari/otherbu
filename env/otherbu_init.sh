4588510059a65fc658db48f8e9ccdd5611ae3b4e
Fujimisakari <fujimisakari@gmail.com>
2017-01-10 11:23:40 +0900

    Improve otherbu_init

--- env/otherbu_init.sh
+++ env/otherbu_init.sh
@@ -1,60 +1,61 @@
 #!/bin/bash
 
-source application/settings/private_config.py
-
-echo '##### Initialize Start #####'
+echo "##### Initialize Start #####"
 
 # For Production
-if [ $1 -a $1 = 'production' ]; then
-    echo '===== DB Setup ====='
-    echo 'GRANT ALL PRIVILEGES ON *.* to ${DB_USER}@localhost identified BY "${DB_PASS}";' | mysql -uroot -p
-    echo 'DROP DATABASE IF EXISTS ${DB_NAME};' | mysql -u${DB_USER} -p${DB_PASS}
-    echo 'CREATE DATABASE ${DB_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;' | mysql -u${DB_USER} -p${DB_PASS}
+if [ $1 -a $1 = "production" ]; then
+    source .env.production
+    echo "===== Production DB Setup ====="
+    echo "GRANT ALL PRIVILEGES ON *.* to $DB_USER@'%' identified BY '$DB_PASS';" | mysql -uroot -p -h $DB_HOST
+    echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
+    echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
 
     python ../manage.py syncdb --noinput
     python ../manage.py migrate
 
-    echo '===== Load Master Data ====='
-    mysql -u${DB_USER} -p${DB_PASS} ${DB_NAME} < fixtures/master_data.sql
+    echo "===== Load Master Data ====="
+    mysql -u$DB_USER -p$DB_PASS $DB_NAME -h $DB_HOST < fixtures/master_data.sql
 fi
 
 # For Staging
-if [ $1 -a $1 = 'staging' ]; then
-    echo '===== Dev DB Setup ====='
-    echo 'GRANT ALL PRIVILEGES ON *.* to ${DB_USER}@localhost identified BY "${DB_PASS}";' | mysql -uroot -p
-    echo 'DROP DATABASE IF EXISTS ${DEV_DB_NAME};' | mysql -u${DEV_DB_USER} -p${DEV_DB_PASS}
-    echo 'CREATE DATABASE ${DEV_DB_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;' | mysql -u${DEV_DB_USER} -p${DEV_DB_PASS}
+if [ $1 -a $1 = "staging" ]; then
+    source .env.staging
+    echo "===== Staging DB Setup ====="
+    echo "GRANT ALL PRIVILEGES ON *.* to $DB_USER@'%' identified BY '$DB_PASS';" | mysql -uroot -p -h $DB_HOST
+    echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
+    echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
 
     python application/manage.py syncdb --noinput
     python application/manage.py migrate
 
-    echo '===== Load Master Data ====='
-    mysql -u${DEV_DB_USER} -p${DEV_DB_PASS} ${DEV_DB_NAME} < fixtures/master_data.sql
+    echo "===== Load Master Data ====="
+    mysql -u$DB_USER -p$DB_PASS $DB_NAME -h $DB_HOST < fixtures/master_data.sql
 fi
 
 # For Develop
-if [ $1 -a $1 = 'develop' ]; then
-    echo '===== Create User Directory For Debug ====='
+if [ $1 -a $1 = "develop" ]; then
+    source .env
+    echo "===== Create User Directory For Debug ====="
     cp -rf fixtures/debug.user static/user/facebook
 
-    if [ $2 -a $2 = 'pip' ]; then
-        echo '===== Pip Install ====='
+    if [ $2 -a $2 = "pip" ]; then
+        echo "===== Pip Install ====="
         pip install -r requirements.txt
     fi
 
-    echo '===== Local DB Setup ====='
-    echo 'GRANT ALL PRIVILEGES ON *.* to ${DB_USER}@localhost identified BY "${DB_PASS}";' | mysql -uroot -p
-    echo 'DROP DATABASE IF EXISTS ${DB_NAME};' | mysql -u${DB_USER} -p${DB_PASS}
-    echo 'CREATE DATABASE ${DB_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;' | mysql -u${DB_USER} -p${DB_PASS}
+    echo "===== Develop DB Setup ====="
+    echo "GRANT ALL PRIVILEGES ON *.* to $DB_USER@'%' identified BY '$DB_PASS';" | mysql -uroot -p -h $DB_HOST
+    echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
+    echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
 
     python application/manage.py syncdb --noinput
     python application/manage.py migrate
 
-    echo '===== Load Master Data ====='
-    mysql -u${DB_USER} -p${DB_PASS} ${DB_NAME} < fixtures/master_data.sql
+    echo "===== Load Master Data ====="
+    mysql -u$DB_USER -p$DB_PASS $DB_NAME -h $DB_HOST < fixtures/master_data.sql
 
-    echo '===== Load Debug Data ====='
-    mysql -u${DB_USER} -p${DB_PASS} ${DB_NAME} < fixtures/debug_data.sql
+    echo "===== Load Debug Data ====="
+    mysql -u$DB_USER -p$DB_PASS $DB_NAME -h $DB_HOST < fixtures/debug_data.sql
 fi
 
-echo '##### Initialize Done #####'
+echo "##### Initialize Done #####"
