b1fa08c5236e1c4c1841e930851881c5a631c232
Fujimisakari <fujimisakari@gmail.com>
2017-01-17 22:48:58 +0900

    Update log setting

--- application/settings/dev_settings.py
+++ application/settings/dev_settings.py
@@ -5,5 +5,3 @@ from settings.base_settings import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 AUTO_LOGIN = DEBUG
-
-STATIC_URL = 'https://s3-ap-northeast-1.amazonaws.com/static-otherbu-prod/'
--- application/settings/staging_settings.py
+++ application/settings/staging_settings.py
@@ -5,3 +5,5 @@ from settings.base_settings import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 AUTO_LOGIN = False
+
+STATIC_URL = 'https://s3-ap-northeast-1.amazonaws.com/static-otherbu-prod/'
