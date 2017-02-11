e461e4372626f49fe3b645f71b44edbbcccd6328
Fujimisakari <fujimisakari@gmail.com>
2017-02-11 21:57:46 +0900

    fix init.sh

--- env/init.sh
+++ env/init.sh
@@ -33,8 +33,8 @@ if [ $1 -a $1 = "develop" ]; then
     source .env.develop
 
     echo "===== DB Setup ====="
-    echo "GRANT ALL PRIVILEGES ON *.* to $DB_USER@'%' identified BY '$DB_PASS';" | mysql -uroot -p -h $DB_HOST
-    echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
+    # echo "GRANT ALL PRIVILEGES ON *.* to $DB_USER@'%' identified BY '$DB_PASS';" | mysql -uroot -p -h $DB_HOST
+    # echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
     echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
 
     initialize
@@ -43,7 +43,7 @@ if [ $1 -a $1 = "develop" ]; then
     mysql -u$DB_USER -p$DB_PASS $DB_NAME -h $DB_HOST < fixtures/debug_data.sql
 
     echo "===== Create User Directory For Debug ====="
-    cp -rf fixtures/debug.user static/user/facebook
+    cp -rf fixtures/debug.user media/user/facebook
 fi
 
 # For Local
@@ -51,8 +51,8 @@ if [ $1 -a $1 = "local" ]; then
     source .env.local
 
     echo "===== DB Setup ====="
-    echo "GRANT ALL PRIVILEGES ON *.* to $DB_USER@'%' identified BY '$DB_PASS';" | mysql -uroot -p -h $DB_HOST
-    echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
+    # echo "GRANT ALL PRIVILEGES ON *.* to $DB_USER@'%' identified BY '$DB_PASS';" | mysql -uroot -p -h $DB_HOST
+    # echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
     echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
 
     initialize
