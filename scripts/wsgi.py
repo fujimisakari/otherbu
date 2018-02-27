c8704c0dab2f51f7e03159d6ee7b5de4671ac908
Fujimisakari <fujimisakari@gmail.com>
2018-02-28 03:19:03 +0900

    update wsgi.py

--- scripts/wsgi.py
+++ scripts/wsgi.py
@@ -1,12 +1,19 @@
 import os
 import sys
 
+import dotenv
 from django.core.wsgi import get_wsgi_application
+
+current_path = os.path.dirname(__file__)
+
+if os.environ.get('APP_ENV') and os.environ['APP_ENV'] == 'production':
+    dotenv.load_dotenv(os.path.join(current_path, '../.env_production'))
+else:
+    dotenv.load_dotenv(os.path.join(current_path, '../.env_develop'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'application.settings')
 
 paths = ('../application',)
-current_path = os.path.dirname(__file__)
 for path in paths:
     sys.path.insert(0, os.path.abspath(os.path.join(current_path, path)))
 
